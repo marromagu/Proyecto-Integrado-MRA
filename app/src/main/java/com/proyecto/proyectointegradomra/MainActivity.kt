19cf5f311fd04a94db2c1e77195fabd2682e1df6
Mario Romero Aguilar <mromagu2611@g.educaand.es>
2024-10-02 23:26:42 +0200

    Establecer la arquitectura MVVM

---
+++ app/src/main/java/com/proyecto/proyectointegradomra/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.proyecto.proyectointegradomra
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.proyecto.proyectointegradomra.ui.theme.ProyectoIntegradoMRATheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProyectoIntegradoMRATheme {
+
+            }
+        }
+    }
+}
+
