2797f501952323d027e0e25826cfee2aeb655f7f
Mario Romero Aguilar <mromagu2611@g.educaand.es>
2024-10-05 18:06:28 +0200

    Implementación de las librerias y dependencias de Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ android {
 
 dependencies {
 
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
