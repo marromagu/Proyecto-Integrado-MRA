455f3ab58a529e68d0d9d83013229ddf324b9510
Mario Romero Aguilar <mromagu2611@g.educaand.es>
2024-10-09 21:58:50 +0200

    Convirtiendo objetos de AppScreens (las rutas a las ventanas)en data object por recomendacion del ID

--- app/src/main/java/com/proyecto/proyectointegradomra/navigation/AppScreens.kt
+++ app/src/main/java/com/proyecto/proyectointegradomra/navigation/AppScreens.kt
@@ -1,8 +1,8 @@
 package com.proyecto.proyectointegradomra.navigation
 
 sealed class AppScreens(val ruta: String){
-    object StartScreen: AppScreens("start_screen")
-    object LogInScreen: AppScreens("login_screen")
-    object SignUpScreen: AppScreens("signup_screen")
-    object HomeScreen: AppScreens("home_screen")
+    data object StartScreen: AppScreens("start_screen")
+    data object LogInScreen: AppScreens("login_screen")
+    data object SignUpScreen: AppScreens("signup_screen")
+//    data object HomeScreen: AppScreens("home_screen")
 }
